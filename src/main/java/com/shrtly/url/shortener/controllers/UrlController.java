e25f724f9315c2392bc0a1a1033c38468546b137
alvyynm <alvyynm@gmail.com>
2024-08-26 07:50:40 +0300

    feat: create /shorten POST route

--- src/main/java/com/shrtly/url/shortener/controllers/UrlController.java
+++ src/main/java/com/shrtly/url/shortener/controllers/UrlController.java
@@ -1,7 +1,12 @@
 package com.shrtly.url.shortener.controllers;
 
+import com.shrtly.url.shortener.models.Url;
 import com.shrtly.url.shortener.services.UrlService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -20,6 +25,13 @@ public class UrlController {
         String key = "message";
         String value = "Hello World!";
         return Map.of(key, value);
+    }
+
+    @PostMapping("/shorten")
+    public Object shortenUrl(@RequestBody Map<String, String> payload) {
+        String originalUrl = payload.get("originalUrl");
+        Url responseData = urlService.createUrl(originalUrl);
+        return new ResponseEntity<>(responseData, HttpStatus.CREATED);
     }
 
 }
