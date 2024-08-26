9fa652cc961d093ac8ad8221c0906a893debed70
alvyynm <alvyynm@gmail.com>
2024-08-26 06:49:17 +0300

    feat: create UrlController

--- src/main/java/com/shrtly/url/shortener/controllers/UrlController.java
+++ src/main/java/com/shrtly/url/shortener/controllers/UrlController.java
@@ -1,4 +1,21 @@
 package com.shrtly.url.shortener.controllers;
 
+import com.shrtly.url.shortener.services.UrlService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UrlController {
+
+    private final UrlService urlService;
+
+    public UrlController(UrlService urlService) {
+        this.urlService = urlService;
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+
 }
