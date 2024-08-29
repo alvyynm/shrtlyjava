7468392a9160097a166de01406c170abd06e0c5b
alvyynm <alvyynm@gmail.com>
2024-08-29 13:10:14 +0300

    feat: create UrlStat repo

--- src/main/java/com/shrtly/url/shortener/repository/UrlStatsRepository.java
+++ src/main/java/com/shrtly/url/shortener/repository/UrlStatsRepository.java
@@ -1,4 +1,11 @@
 package com.shrtly.url.shortener.repository;
 
-public interface UrlStatsRepository {
+import com.shrtly.url.shortener.models.UrlStat;
+import org.springframework.data.repository.CrudRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface UrlStatsRepository extends CrudRepository<UrlStat, Integer> {
+    List<UrlStat> findByUrlIdAndDate(Integer urlId, LocalDate date);
 }
