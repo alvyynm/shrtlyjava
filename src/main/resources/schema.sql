b6ef9dabfcc5d8347a0339d4cedc22095fe2c95f
alvyynm <alvyynm@gmail.com>
2024-08-29 13:10:57 +0300

    feat: create UrlStat db initialization sql statement

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,11 @@ create table if not exists urls (
     original_url varchar(255),
     shortened_url varchar(255)
 );
+
+create table if not exists urlstats (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    url_id INT NOT NULL,
+    date DATE NOT NULL,
+    click_count INT NOT NULL,
+    FOREIGN KEY (url_id) REFERENCES urls(id)
+)
