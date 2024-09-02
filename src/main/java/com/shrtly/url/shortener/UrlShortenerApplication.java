451c22da7fa8276cfd32bcf7b54d59ace89a13a9
alvyynm <alvyynm@gmail.com>
2024-09-02 19:18:10 +0300

    feat: enable rsakprop class as config

--- src/main/java/com/shrtly/url/shortener/UrlShortenerApplication.java
+++ src/main/java/com/shrtly/url/shortener/UrlShortenerApplication.java
@@ -1,8 +1,11 @@
 package com.shrtly.url.shortener;
 
+import com.shrtly.url.shortener.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class UrlShortenerApplication {
 
