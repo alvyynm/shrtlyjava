94ac4b1a37e8af25defc8c48ea5c267588c66e69
alvyynm <alvyynm@gmail.com>
2024-09-03 14:49:19 +0300

    feat: create User model

--- src/main/java/com/shrtly/url/shortener/models/User.java
+++ src/main/java/com/shrtly/url/shortener/models/User.java
@@ -1,4 +1,58 @@
 package com.shrtly.url.shortener.models;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("USERS")
 public class User {
+    @Id
+    private Integer userId;
+
+    private String email;
+
+    private String password;
+
+    private String fullName;
+
+    private String userRole;
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 }
