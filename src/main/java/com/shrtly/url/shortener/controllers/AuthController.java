69e60a0a892865435c47494795983505490332f1
alvyynm <alvyynm@gmail.com>
2024-09-03 13:36:10 +0300

    feat: create placeholder routes for auth

--- src/main/java/com/shrtly/url/shortener/controllers/AuthController.java
+++ src/main/java/com/shrtly/url/shortener/controllers/AuthController.java
@@ -4,10 +4,10 @@ import com.shrtly.url.shortener.services.TokenService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1")
 public class AuthController {
 
     private static final Logger LOG = LoggerFactory.getLogger(AuthController.class);
@@ -24,5 +24,30 @@ public class AuthController {
         String token = tokenService.generateToken(authentication);
         LOG.debug("Token granted {}", token);
         return token;
+    }
+
+    @PostMapping("/auth/signup")
+    public String signup(Authentication authentication) {
+        //TODO: save user details to db and return them
+        return "";
+    }
+
+    @PostMapping("/auth/login")
+    public String login(Authentication authentication) {
+        // TODO: authenticate user and return jwt session
+        return "jwt";
+    }
+
+    @GetMapping("/auth/user/{id}")
+    public String getUserDetails(@PathVariable String id) {
+        // TODO: Search for user with given id within db and return their info
+        // User must be authenticated
+        return "user";
+    }
+
+    @GetMapping("/auth/users")
+    public String getUsers() {
+        // TODO: Get all users (ADMIN only)
+        return "users";
     }
 }
