baceb7015e71e188b5c3fcc06958136d43f07538
alvyynm <alvyynm@gmail.com>
2024-09-03 14:50:05 +0300

    feat: create sql statement for creating users db

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,4 +11,14 @@ create table if not exists urlstats (
     date DATE NOT NULL,
     click_count INT NOT NULL,
     FOREIGN KEY (url_id) REFERENCES urls(id)
-)
+);
+
+create table if not exists users (
+    userId INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL,
+    fullName VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    userRole VARCHAR(10) DEFAULT 'USER',
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
