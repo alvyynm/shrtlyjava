6ff87fc46b3a5d594104b334cb49d5f0b1c600ff
alvyynm <alvyynm@gmail.com>
2024-09-03 16:28:51 +0300

    chore: rename UserDto to UserSignupDto

--- src/main/java/com/shrtly/url/shortener/dtos/UserDto.java
+++
@@ -1,4 +0,0 @@
-package com.shrtly.url.shortener.dtos;
-
-public class UserDto {
-}
---
+++ src/main/java/com/shrtly/url/shortener/dtos/UserSignupDto.java
@@ -0,0 +1,19 @@
+package com.shrtly.url.shortener.dtos;
+
+public class UserSignupDto {
+    private String email;
+    private String password;
+    private String fullName;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+}
