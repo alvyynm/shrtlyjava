f3b799077ca46ace4d3213eb6bc1a00f8a605e16
alvyynm <alvyynm@gmail.com>
2024-09-04 15:56:58 +0300

    feat: define custom findByEmail method in UserRepository

--- src/main/java/com/shrtly/url/shortener/repository/UserRepository.java
+++ src/main/java/com/shrtly/url/shortener/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.shrtly.url.shortener.repository;
 import com.shrtly.url.shortener.models.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
 }
