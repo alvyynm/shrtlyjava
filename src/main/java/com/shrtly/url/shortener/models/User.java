ecdb3c96edc25f5b926fc9229f75a1dfd3f5a3a4
alvyynm <alvyynm@gmail.com>
2024-09-04 16:22:30 +0300

    feat: implement UserDetails interface in User model

--- src/main/java/com/shrtly/url/shortener/models/User.java
+++ src/main/java/com/shrtly/url/shortener/models/User.java
@@ -2,9 +2,14 @@ package com.shrtly.url.shortener.models;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Table("USERS")
-public class User {
+public class User implements UserDetails {
     @Id
     private Integer userId;
 
@@ -50,12 +55,42 @@ public class User {
         this.email = email;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
     public String getPassword() {
         return password;
     }
 
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public String getFullName() {
