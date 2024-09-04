d94a6760af5ebe2393a49ce894843d0ea29be9d5
alvyynm <alvyynm@gmail.com>
2024-09-04 16:17:11 +0300

    chore: remove unused code

--- src/main/java/com/shrtly/url/shortener/UrlShortenerApplication.java
+++ src/main/java/com/shrtly/url/shortener/UrlShortenerApplication.java
@@ -1,11 +1,9 @@
 package com.shrtly.url.shortener;
 
-import com.shrtly.url.shortener.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(RsaKeyProperties.class)
+
 @SpringBootApplication
 public class UrlShortenerApplication {
 
