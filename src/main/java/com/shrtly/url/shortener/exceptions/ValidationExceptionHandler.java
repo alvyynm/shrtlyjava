3167054c7760ce68a9889baa2c96a5b9affab835
alvyynm <alvyynm@gmail.com>
2024-09-05 16:30:35 +0300

    feat: create validation exception handler

--- src/main/java/com/shrtly/url/shortener/exceptions/ValidationExceptionHandler.java
+++ src/main/java/com/shrtly/url/shortener/exceptions/ValidationExceptionHandler.java
@@ -1,4 +1,28 @@
 package com.shrtly.url.shortener.exceptions;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestControllerAdvice
 public class ValidationExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> notValid(MethodArgumentNotValidException ex, HttpServletRequest request) {
+        List<String> errors = new ArrayList<>();
+
+        ex.getAllErrors().forEach(error -> errors.add(error.getDefaultMessage()));
+
+        Map<String, List<String>> responseErrors = new HashMap<>();
+        responseErrors.put("errors", errors);
+
+        return new ResponseEntity<>(responseErrors, HttpStatus.BAD_REQUEST);
+    }
 }
