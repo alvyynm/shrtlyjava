81edf9ef3db46bb23d3121f4b827356e65036d91
alvyynm <alvyynm@gmail.com>
2024-09-05 16:39:08 +0300

    fix: properly validate LoginUserDTO

--- src/main/java/com/shrtly/url/shortener/dtos/LoginUserDto.java
+++ src/main/java/com/shrtly/url/shortener/dtos/LoginUserDto.java
@@ -3,13 +3,14 @@ package com.shrtly.url.shortener.dtos;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 
 public class LoginUserDto {
-    @Email
-    @NotEmpty
+    @NotEmpty(message = "email is required")
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "The email is not a valid email")
     private String email;
 
-    @NotEmpty
+    @NotEmpty(message = "password is required")
     private String password;
 
     public String getEmail() {
