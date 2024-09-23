a5b4e4c5e4e2c4655089dad5ea958d6d134d95ef
alvyynm <alvyynm@gmail.com>
2024-09-23 17:30:59 +0300

    feat: fixes #9

--- src/main/java/com/shrtly/url/shortener/controllers/UserController.java
+++ src/main/java/com/shrtly/url/shortener/controllers/UserController.java
@@ -1,12 +1,17 @@
 package com.shrtly.url.shortener.controllers;
 
+import com.shrtly.url.shortener.models.User;
 import com.shrtly.url.shortener.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -27,6 +32,13 @@ public class UserController {
     @GetMapping("/auth/users")
     public ResponseEntity<UrlController.CommonApiResponse> getUsers() {
         // TODO: Get all users (ADMIN only)
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        User currentUser = (User) authentication.getPrincipal();
+
+        if (!Objects.equals(currentUser.getUserRole(), "ADMIN")){
+            return new ResponseEntity<>(new UrlController.CommonApiResponse(false, "Unauthorized", null), HttpStatus.FORBIDDEN);
+        }
         return new ResponseEntity<>(new UrlController.CommonApiResponse(true, "Users found", userService.findAll()), HttpStatus.OK);
     }
 
