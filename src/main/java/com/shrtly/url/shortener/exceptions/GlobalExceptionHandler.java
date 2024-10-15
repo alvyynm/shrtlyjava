fdce4090684a76352f6bc960021dd8ebbd2c3e7f
alvyynm <alvyynm@gmail.com>
2024-10-15 17:10:21 +0300

    create global exception handler

---
+++ src/main/java/com/shrtly/url/shortener/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.shrtly.url.shortener.exceptions;
+
+import io.jsonwebtoken.JwtException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<?> handleJwtException(JwtException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("error", "Invalid or expired token"));
+    }
+
+    // Handle generic authentication exceptions
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<?> handleAuthenticationException(AuthenticationException ex) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(Map.of("error", "Unauthorized access: " + ex.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGenericException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).
+                body(Map.of("error", "An error occurred: " + ex.getMessage()));
+    }
+}
