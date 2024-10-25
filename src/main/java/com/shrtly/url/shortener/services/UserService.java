fb6bfe2e2a22285a5be74eb465ed3b696696016e
alvyynm <alvyynm@gmail.com>
2024-10-25 13:25:35 +0300

    refactor: remove unused imports and code

--- src/main/java/com/shrtly/url/shortener/services/UserService.java
+++ src/main/java/com/shrtly/url/shortener/services/UserService.java
@@ -4,17 +4,12 @@ import com.shrtly.url.shortener.dtos.LoginUserDto;
 import com.shrtly.url.shortener.dtos.UserSignupDto;
 import com.shrtly.url.shortener.models.User;
 import com.shrtly.url.shortener.repository.UserRepository;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -23,7 +18,6 @@ import java.util.Optional;
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-//    private final AuthenticationManager authenticationManager;
     private final AuthenticationProvider authenticationProvider;
 
     public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationProvider authenticationProvider) {
@@ -79,7 +73,6 @@ public class UserService {
 
     public User login(LoginUserDto loginPayload) {
         Authentication authentication = new UsernamePasswordAuthenticationToken(loginPayload.getEmail(), loginPayload.getPassword());
-//        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginPayload.getEmail(), loginPayload.getPassword()));
         authenticationProvider.authenticate(authentication); // authenticating the user
         return userRepository.findByEmail(loginPayload.getEmail()).orElseThrow();
     }
